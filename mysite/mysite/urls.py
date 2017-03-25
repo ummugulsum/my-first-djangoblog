ebbad49b1e3ce623e94d47b2edfe3825c6d80f33
ummugulsum <ummugulsum.unluer@gmail.com>
2017-03-25 17:48:45 +0300

    hata düzeltildi

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-urlpatterns = {
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'',include('blog.urls'))
-}
+]
